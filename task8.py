f062c7d487e8d1cecb85e05cc3add1bca77081d3
shahareng <shaharbel0@gmail.com>
2020-12-15 19:16:11 +0200

    update task8

--- task8.py
+++ task8.py
@@ -1,7 +1,5 @@
 #Shahar Engel
 
-from doctest import testmod
-
 class Uniform:
     # low- float
     # high- float
@@ -12,23 +10,21 @@ class Uniform:
 
 
 def max_revenue_auction1(agent1, value1): #agent1: Uniform, value1:float
-    '''
+    """
     >>> max_revenue_auction1(Uniform(10,30), 17)
     Agent chosen and pays 15
-    ok
     >>> max_revenue_auction1(Uniform(20,40), 20.91)
     Agent chosen and pays 20
-    ok
     >>> max_revenue_auction1(Uniform(10,30), 10)
-    Agent chosen and pays 15
-    failed
-    '''
+    Agent doesn't chosen
+    """
 
     high = agent1.high
     treshold = high/2;
     rv = (2*value1)-high
+    pay = max(treshold, agent1.low)
     if rv > 0:
-        print("Agent chosen and pays " + str(treshold))
+        print("Agent chosen and pays " + str(pay))
     else:
         print("Agent doesn't chosen")
 
@@ -37,13 +33,10 @@ def max_revenue_auction2(agent1, agent2, value1, value2): #agent1: Uniform, agen
     '''
     >>> max_revenue_auction2(Uniform(10,30), Uniform(20,40), 23,27)
     Agent 1 wins and pays 22
-    ok
     >>> max_revenue_auction2(Uniform(10,30), Uniform(20,40), 15.33, 30.35)
     Agent 2 wins and pays 20.33
-    ok
-    >>> max_revenue_auction2(Uniform(10,30), Uniform(20,40), 23,27)
-    Agent 2 wins and pays 15
-    failed
+    >>> max_revenue_auction2(Uniform(10,30), Uniform(20,40), 29.22, 20.61)
+    Agent 1 wins and pays 15.61
     '''
 
 
@@ -67,10 +60,12 @@ def max_revenue_auction2(agent1, agent2, value1, value2): #agent1: Uniform, agen
 #main
 agent1 = Uniform(10,30)
 agent2 = Uniform(20,40)
-max_revenue_auction1(agent1, 15.01)
-max_revenue_auction2(agent1, agent2, 15.33, 30.35)
+# max_revenue_auction1(agent1, 31)
+# max_revenue_auction2(agent1, agent2, 29.22, 20.61)
+# max_revenue_auction2(agent1, agent2, 27.44, 39.51)
 
 # call the testmod function
-# if __name__ == '__main__':
-   # testmod(name='max_revenue_auction1', verbose=True)
-   # testmod(name='max_revenue_auction2', verbose=True)
+if __name__ == '__main__':
+    import doctest
+    (failures, tests) = doctest.testmod(report=True)
+    print ("{} failures, {} tests".format(failures, tests))
